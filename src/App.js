acfca492bb89f76bbe4580ab59268ab7ee85bb9c
David Amunga <david.onyimbo@gmail.com>
2021-09-25 22:01:01 +0300

    Added Authentication using Auth0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-9xl font-bold text-yellow-400">Hello World!</h1>
-    </div>
-  );
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  if (isAuthenticated) {
+    return (
+      <div>
+        <h1 className="text-9xl font-bold text-yellow-400">Logged In !</h1>
+        <button onClick={() => logout()}>Log Out</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1 className="text-9xl font-bold text-yellow-400">Hello World!</h1>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      </div>
+    );
+  }
 }
 
 export default App;
